41cb49f571faae4d5fed63819bca401f0ccc5ad3
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-03 17:49:49 +0330

    create: simple and primary controller created

---
+++ src/main/java/org/rahmasir/learnspringsecurity/ContentController.java
@@ -0,0 +1,23 @@
+package org.rahmasir.learnspringsecurity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContentController {
+
+    @GetMapping("/home")
+    public String handleWelcome() {
+        return "home";
+    }
+
+    @GetMapping("/user/home")
+    public String handleUserHome() {
+        return "home_user";
+    }
+
+    @GetMapping("/admin/home")
+    public String handleAdminHome() {
+        return "admin_home";
+    }
+}
