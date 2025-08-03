0c4e1bef3bd03695473e5e133800c0e934c3591f
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-03 23:10:05 +0330

    update: environment variables added

--- src/main/java/org/rahmasir/learnspringsecurity/LearnSpringSecurityApplication.java
+++ src/main/java/org/rahmasir/learnspringsecurity/LearnSpringSecurityApplication.java
@@ -1,5 +1,7 @@
 package org.rahmasir.learnspringsecurity;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnSpringSecurityApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().filename(".env").load();
+        for (DotenvEntry entry : dotenv.entries()) {
+            if (System.getProperty(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        }
+
         SpringApplication.run(LearnSpringSecurityApplication.class, args);
     }
 
