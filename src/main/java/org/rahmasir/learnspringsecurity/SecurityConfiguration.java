f5efa0d44122ee37b976089c7d7e098e000db95a
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-03 22:59:00 +0330

    update: security configuration - user detail service and an encoder added

--- src/main/java/org/rahmasir/learnspringsecurity/SecurityConfiguration.java
+++ src/main/java/org/rahmasir/learnspringsecurity/SecurityConfiguration.java
@@ -4,6 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +24,26 @@ public class SecurityConfiguration {
             registry.requestMatchers("/user/**").hasRole("USER");
             registry.requestMatchers("/admin/**").hasRole("ADMIN");
             registry.anyRequest().authenticated();
-        }).build();
+        }).formLogin(AbstractAuthenticationFilterConfigurer::permitAll).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails normalUser = User.builder()
+                .username("root")
+                .password("$2a$12$y4kjv39Ox4rHGEUXeeBoNeQsBrXoY9Iln3RnZZ6v9B8chqeFrvGiW")
+                .roles("USER")
+                .build();
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password("$2a$12$yCplHcG.FwCiR7dyNzrE6u6bRZKHMjuU/fCpcnM7NA2T1ZbMlwDjy")
+                .roles("ADMIN", "USER")
+                .build();
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
     }
 }
