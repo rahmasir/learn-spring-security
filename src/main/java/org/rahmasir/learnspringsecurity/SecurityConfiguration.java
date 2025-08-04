9e3aa8dbdad2f7126e319347762f45e3ee95f19c
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-04 10:12:41 +0330

    update: registeration controller added + configuration sec. conf.

--- src/main/java/org/rahmasir/learnspringsecurity/SecurityConfiguration.java
+++ src/main/java/org/rahmasir/learnspringsecurity/SecurityConfiguration.java
@@ -1,10 +1,14 @@
 package org.rahmasir.learnspringsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,14 +21,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(registry -> {
-            registry.requestMatchers("/home").permitAll();
-            registry.requestMatchers("/user/**").hasRole("USER");
-            registry.requestMatchers("/admin/**").hasRole("ADMIN");
-            registry.anyRequest().authenticated();
-        }).formLogin(AbstractAuthenticationFilterConfigurer::permitAll).build();
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/home", "/register/user").permitAll();
+                    registry.requestMatchers("/user/**").hasRole("USER");
+                    registry.requestMatchers("/admin/**").hasRole("ADMIN");
+                    registry.anyRequest().authenticated();
+                }).formLogin(AbstractAuthenticationFilterConfigurer::permitAll).build();
     }
 
     @Bean
@@ -32,18 +41,31 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails normalUser = User.builder()
+//                .username("root")
+//                .password("$2a$12$y4kjv39Ox4rHGEUXeeBoNeQsBrXoY9Iln3RnZZ6v9B8chqeFrvGiW")
+//                .roles("USER")
+//                .build();
+//        UserDetails adminUser = User.builder()
+//                .username("admin")
+//                .password("$2a$12$yCplHcG.FwCiR7dyNzrE6u6bRZKHMjuU/fCpcnM7NA2T1ZbMlwDjy")
+//                .roles("ADMIN", "USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(normalUser, adminUser);
+//    }
+
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails normalUser = User.builder()
-                .username("root")
-                .password("$2a$12$y4kjv39Ox4rHGEUXeeBoNeQsBrXoY9Iln3RnZZ6v9B8chqeFrvGiW")
-                .roles("USER")
-                .build();
-        UserDetails adminUser = User.builder()
-                .username("admin")
-                .password("$2a$12$yCplHcG.FwCiR7dyNzrE6u6bRZKHMjuU/fCpcnM7NA2T1ZbMlwDjy")
-                .roles("ADMIN", "USER")
-                .build();
-        return new InMemoryUserDetailsManager(normalUser, adminUser);
+        return  userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
     }
 }
